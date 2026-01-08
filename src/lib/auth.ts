4e348cbef87f1f55c652dbd23acbdf066f68f092
Rafael Aguiar <terule@gmail.com>
2026-01-08 10:12:07 -0300

    ✨ (auth-client.ts): introduce new auth client module to simplify authentication handling ♻️ (auth.ts): refactor auth configuration for better readability and maintainability

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,9 +5,10 @@ import prisma from '@/lib/prisma'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: 'postgresql', // or "mysql", "postgresql", ...etc
+    provider: 'postgresql',
   }),
   emailAndPassword: {
     enabled: true,
   },
+  
 })
